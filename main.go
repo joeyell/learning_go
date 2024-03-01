ec17c947ab0d0a0aa48374f060af18f3fcf6ab0c
joeyelland@gmail.com <joeyelland@gmail.com>
2024-03-01 19:11:17 +0000

    another exercism task

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"learning_go/Exercism/go/greeting"
 	"learning_go/Exercism/go/lasagna"
+	"learning_go/Exercism/go/partyrobot"
 )
 
 func main() {
 	fmt.Println(greeting.HelloWorld())
 	fmt.Println(lasagna.ElapsedTime(1, 30))
+	fmt.Println(partyrobot.AssignTable("Christiane", 27, "Frank", "on the left", 23.7834298))
 }
